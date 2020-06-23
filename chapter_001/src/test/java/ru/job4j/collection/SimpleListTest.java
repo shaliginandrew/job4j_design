db93c2ca8e3f7653bbb558b6a1fd102ac8ba3d1c
shaliginandrew <shaligin.andrew@gmail.com>
2020-06-23 21:31:11 +0300

    Создать контейнер на базе связанного списка [#294170]

--- chapter_001/src/test/java/ru/job4j/collection/SimpleListTest.java
+++ chapter_001/src/test/java/ru/job4j/collection/SimpleListTest.java
@@ -5,22 +5,34 @@ import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
 
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class  SimpleListTest {
     @Test
     public void getIndex() {
-        SimpleList node = new SimpleList(1);
+        SimpleList<Integer> node = new SimpleList(1);
         node.add(2);
         node.add(3);
         node.add(4);
-        int rsl = (int) node.get(2);
+        int rsl = node.get(2);
         assertThat(rsl, is(3));
     }
+
     @Test
     public void whenAddThenIt() {
-        SimpleList node = new SimpleList(1);
-        node.add(2);
-        int rsl = (int) node.iterator().next();
-        assertThat(rsl, is(2));
+        SimpleList<String> node = new SimpleList("first");
+        String rsl = node.iterator().next();
+        assertThat(rsl, is("first"));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenCorruptedIt() {
+        SimpleList<String> node = new SimpleList("first");
+        Iterator<String> it = node.iterator();
+        node.add("second");
+        it.next();
     }
 }
 
