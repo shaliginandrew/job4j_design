2924811cb819dde41a1c94b4a010d0ae4d155ec6
shaliginandrew <shaligin.andrew@gmail.com>
2020-07-02 21:16:34 +0300

    Создать элементарную структуру дерева [#294203]

---
+++ chapter_001/src/test/java/ru/job4j/tree/TreeTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.tree;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+
+public class TreeTest {
+    @Test
+    public void when6ElFindLastThen6() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(
+                tree.findBy(6).isPresent(),
+                is(true)
+        );
+    }
+
+    @Test
+    public void when6ElFindNotExitThenOptionEmpty() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        assertThat(
+                tree.findBy(7).isPresent(),
+                is(false)
+        );
+    }
+
+    @Test
+    public void test3() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(4, 2);
+        assertThat(
+                tree.findBy(4).isPresent(),
+                is(false)
+        );
+    }
+}
