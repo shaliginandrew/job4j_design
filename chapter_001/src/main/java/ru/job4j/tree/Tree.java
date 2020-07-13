89e9fb916209f00888ea95ffc1a88c489b45772c
shaliginandrew <shaligin.andrew@gmail.com>
2020-07-13 12:34:20 +0300

    Добавить метод boolean isBinary() [#294204]

--- chapter_001/src/main/java/ru/job4j/tree/Tree.java
+++ chapter_001/src/main/java/ru/job4j/tree/Tree.java
@@ -3,6 +3,7 @@ package ru.job4j.tree;
 import ru.job4j.collection.SimpleList;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 class Tree<E> implements SimpleTree<E> {
     private final Node<E> root;
@@ -29,8 +30,8 @@ class Tree<E> implements SimpleTree<E> {
     }
 
     public boolean isBinary() {
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
+        // Queue<Node<E>> data = new LinkedList<>();
+        //data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
             if (el.children.size() > 2) {
@@ -40,22 +41,23 @@ class Tree<E> implements SimpleTree<E> {
         }
         return true;
     }
-/*
-1. Создаем перменную пустую Node в обертке Optional
-2. Создается очередь data из Node  реализации LinkedList
-3. Добавляем элемент root в очередь( если нельзя добавить вернет false)
-4. До тех пор пока очередь не пустая:
--сначала проверяем root, если не равно искомому - добавляем всех детей root в очередь
--root уже выйдет, буде проверять всех детей поочереди и дети поочереди будут выходить из нее
--если не находим совпадений, то добавляем всех детей тех элементов что выше перебирали и перебираем дальше,
- пока не найдем или очередь не закончится
 
- */
+    /*
+    1. Создаем перменную пустую Node в обертке Optional
+    2. Создается очередь data из Node  реализации LinkedList
+    3. Добавляем элемент root в очередь( если нельзя добавить вернет false)
+    4. До тех пор пока очередь не пустая:
+    -сначала проверяем root, если не равно искомому - добавляем всех детей root в очередь
+    -root уже выйдет, буде проверять всех детей поочереди и дети поочереди будут выходить из нее
+    -если не находим совпадений, то добавляем всех детей тех элементов что выше перебирали и перебираем дальше,
+     пока не найдем или очередь не закончится
+
+     */
     @Override
     public Optional<Node<E>> findBy(E value) {
         Optional<Node<E>> rsl = Optional.empty();
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
+        //  Queue<Node<E>> data = new LinkedList<>();
+        //data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
             if (el.value.equals(value)) {
@@ -66,4 +68,17 @@ class Tree<E> implements SimpleTree<E> {
         }
         return rsl;
     }
+
+    public Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+
+
+        }
+    }
 }
+
+
+
