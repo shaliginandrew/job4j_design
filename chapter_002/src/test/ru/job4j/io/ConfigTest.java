31d2198aecbc9357de5c6f5c49432b01b82abc14
shaliginandrew <shaligin.andrew@gmail.com>
2020-08-04 22:36:18 +0300

    Читаем файл конфигурации [#294221]

---
+++ chapter_002/src/test/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void testOne() {
+        String path = "app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("hibernate.connection.password"),
+                is("password")
+        );
+    }
+
+    @Test
+    public void testTwo() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("name"),
+                is("Petr Arsentev")
+        );
+    }
+
+}
