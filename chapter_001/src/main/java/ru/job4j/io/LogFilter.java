55790a5c45f2beb6401d10d2318b46e71af622f3
shaliginandrew <shaligin.andrew@gmail.com>
2020-08-04 17:52:01 +0300

    BufferedOutputStream [#294219]

--- chapter_001/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_001/src/main/java/ru/job4j/io/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,11 +18,23 @@ public class LogFilter {
         } catch (Exception e) {
         e.printStackTrace();
     }
-    return lines404;
+        return lines404;
+    }
+
+
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out  = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+            for (String list : log) {
+                out.write(list);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        System.out.println(log);
+        save(log, "404.txt");
     }
 }
--- chapter_001/src/main/java/ru/job4j/io/ResultFile.java
+++ chapter_001/src/main/java/ru/job4j/io/ResultFile.java
@@ -1,14 +1,16 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 public class ResultFile {
 
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
-        for (int i = 1; i <= 10; i++) {
+        try (PrintWriter out  = new PrintWriter(new BufferedOutputStream(new FileOutputStream("result.txt")))) {
+            for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
-                out.write((i + "*" + j + "=" + i * j + System.lineSeparator()).getBytes());
+                out.write((i + "*" + j + "=" + i * j + System.lineSeparator()));
             }
         }
     } catch (Exception e) {
