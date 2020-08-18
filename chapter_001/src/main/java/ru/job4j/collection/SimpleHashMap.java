cbfd5260d5cfff101545233bf844f8350fe0ca61
shaliginandrew <shaligin.andrew@gmail.com>
2020-08-18 09:20:03 +0300

    Реализовать собственную структуру данных - HashMap [#294199]

--- chapter_001/src/main/java/ru/job4j/collection/SimpleHashMap.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleHashMap.java
@@ -17,14 +17,22 @@ import java.util.*;
  * Методы разрешения коллизий реализовывать не надо. Например: если при добавлении ключ уже есть, то возвращать false.
  */
 public class SimpleHashMap<K, V> implements Iterable<SimpleHashMap.Node<K, V>> {
-    private Node<K, V>[] table;
-    private int capacity = 16;
-    private double loadFactor = 0.75;
-    private int modCount = 0;
-    private int size = 0;
 
-    public SimpleHashMap() {
-        this.table = new Node[capacity];
+
+    @Override
+    public Iterator<SimpleHashMap.Node<K, V>> iterator() {
+
+        return new Iterator<SimpleHashMap.Node<K, V>>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public SimpleHashMap.Node<K, V> next() {
+                return null;
+            }
+        };
     }
 
 
@@ -40,101 +48,4 @@ public class SimpleHashMap<K, V> implements Iterable<SimpleHashMap.Node<K, V>> {
         }
 
     }
-
-
-    @Override
-    public Iterator<Node<K, V>> iterator() {
-        return new Iterator<Node<K, V>>() {
-            int expectedModCount = modCount;
-            private int it = 0;
-            @Override
-            public boolean hasNext() {
-                boolean result = false;
-                if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
-
-                for (int i = it; i < table.length; i++) {
-                    if (table[i] != null) {
-                        result = true;
-                        it = i;
-                        break;
-                    }
-                }
-                return result;
-            }
-
-            @Override
-            public Node<K, V> next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-               return table[it++];
-
-            }
-        };
-    }
-
-    public boolean insert(K key, V value) {
-        boolean result = false;
-        if (size / table.length < loadFactor) {
-            int hash = key.hashCode() % capacity;
-            if (table[hash] == null) {
-                modCount++;
-                size++;
-                table[hash] = new Node<>(key, value, null);
-                result = true;
-            }
-        } else if (resize()) {
-            int hash = key.hashCode() % capacity;
-            table[hash] = new Node<>(key, value, null);
-            size++;
-            modCount++;
-
-        }
-        return result;
-    }
-
-    public boolean resize() {
-        boolean result = false;
-        if (size / table.length > loadFactor) {
-            Node<K, V>[] oldTable = table;
-            size = 0;
-            capacity *= 2;
-            table = new Node[capacity];
-            for (int i = 0; i < oldTable.length; i++) {
-                if (oldTable[i] != null) {
-                    insert(oldTable[i].key, oldTable[i].value);
-                    result = true;
-                }
-            }
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        SimpleHashMap<Integer, String> map = new SimpleHashMap<>();
-        map.insert(1, "а");
-        map.insert(2, "б");
-        map.insert(3, "в");
-        map.insert(4, "г");
-        map.insert(5, "д");
-        map.insert(6, "е");
-        map.insert(7, "ж");
-        map.insert(8, "з");
-        map.insert(9, "к");
-        map.insert(10, "л");
-        map.insert(11, "м");
-        map.insert(12, "н");
-        map.insert(13, "о");
-        map.insert(14, "п");
-        map.insert(15, "р");
-        map.insert(16, "с");
-        map.insert(17, "т");
-        Iterator<Node<Integer, String>> iterator = map.iterator();
-        while (iterator.hasNext()) {
-            Node<Integer, String> curent = iterator.next();
-            System.out.println(curent.key + " " + curent.value);
-        }
-    }
-    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/collection/SimpleHashMapTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.collection;
+
+
+
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Node;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+
+public class SimpleHashMapTest {
+
+
+
+    @Test
+    public void testIterator() {
+        //SimpleHashMap<Integer, String> map = new SimpleHashMap<>();
+      //  map.insert(1, "а");
+       // map.insert(2, "б");
+      //  map.insert(3, "в");
+      //  Iterator iterator = map.iterator();
+      //  assertThat(iterator.hasNext(), is(true));
+      //  assertThat(iterator.next(), is(map.get(1)));
+      //  //assertThat(iterator.hasNext(), is(true));
+      //  assertThat(iterator.next(), is("б"));
+      //  assertThat(iterator.hasNext(), is(true));
+     //   assertThat(iterator.next(), is("в"));
+     //  assertThat(iterator.hasNext(), is(false));
+      //  assertThat(iterator.next(), is(null));
+
+
+    }
+
+}
+
+
+
+
+
