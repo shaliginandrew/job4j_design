554f6e9513905a0579622fd8c26185e2913bc4e4
shaliginandrew <shaligin.andrew@gmail.com>
2020-08-18 09:23:34 +0300

    Реализовать собственную структуру данных - HashMap [#294199]

--- chapter_001/src/main/java/ru/job4j/collection/SimpleHashMap.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleHashMap.java
@@ -17,20 +17,48 @@ import java.util.*;
  * Методы разрешения коллизий реализовывать не надо. Например: если при добавлении ключ уже есть, то возвращать false.
  */
 public class SimpleHashMap<K, V> implements Iterable<SimpleHashMap.Node<K, V>> {
+    private Node<K, V>[] table;
+    private int capacity = 16;
+    private double loadFactor = 0.75;
+    private int modCount = 0;
+    private int size = 0;
 
+
+    public SimpleHashMap() {
+        this.table = new Node[capacity];
+    }
 
     @Override
     public Iterator<SimpleHashMap.Node<K, V>> iterator() {
 
         return new Iterator<SimpleHashMap.Node<K, V>>() {
+            int expectedModCount = modCount;
+            private int it = 0;
+
             @Override
             public boolean hasNext() {
-                return false;
+                boolean result = false;
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+
+                for (int i = it; i < table.length; i++) {
+                    if (table[i] != null) {
+                        result = true;
+                        it = i;
+                        break;
+                    }
+                }
+                return result;
             }
 
             @Override
             public SimpleHashMap.Node<K, V> next() {
-                return null;
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Node<K, V> result = table[it++];
+                return result;
             }
         };
     }
@@ -47,5 +75,62 @@ public class SimpleHashMap<K, V> implements Iterable<SimpleHashMap.Node<K, V>> {
             this.next = next;
         }
 
+    }
+
+    public boolean insert(K key, V value) {
+        boolean result = false;
+        if (size / table.length < loadFactor) {
+            int hash = key.hashCode() % capacity;
+            if (table[hash] == null) {
+                modCount++;
+                size++;
+                table[hash] = new Node<>(key, value, null);
+                result = true;
+            }
+        } else if (resize()) {
+            int hash = key.hashCode() % capacity;
+            table[hash] = new Node<>(key, value, null);
+            size++;
+            modCount++;
+
+        }
+        return result;
+    }
+
+    public boolean resize() {
+        boolean result = false;
+        if (size / table.length > loadFactor) {
+            Node<K, V>[] oldTable = table;
+            size = 0;
+            capacity *= 2;
+            table = new Node[capacity];
+            for (int i = 0; i < oldTable.length; i++) {
+                if (oldTable[i] != null) {
+                    insert(oldTable[i].key, oldTable[i].value);
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    public V get(K key) throws NoSuchElementException {
+        int hash = key.hashCode() % capacity;
+        if (table[hash] == null) {
+            throw new NoSuchElementException();
+        }
+        return table[hash].value;
+
+    }
+
+    public boolean delete(K key) {
+        boolean result = false;
+        int hash = key.hashCode() % capacity;
+        if (table[hash] == null) {
+            throw new NoSuchElementException();
+        }
+        table[hash] = null;
+        result = true;
+        return result;
     }
 }
