80c159ff159f6e63f5aca849891f0e8810ccac83
shaliginandrew <shaligin.andrew@gmail.com>
2020-08-22 19:27:05 +0300

    File [#294220]

--- chapter_002/src/main/java/ru.job4j/io/Dir.java
+++ chapter_002/src/main/java/ru.job4j/io/Dir.java
@@ -2,9 +2,6 @@ package ru.job4j.io;
 
 import java.io.File;
 
-/**
- * Программа выводит название директорий папки c:\projects и их размер
- */
 public class Dir {
     public static void main(String[] args) {
         File file = new File("c:\\projects");
@@ -15,13 +12,8 @@ public class Dir {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
         System.out.println(String.format("size : %s", file.getTotalSpace()));
-        long length = 0;
         for (File subfile : file.listFiles()) {
-
-                length += subfile.length();
-
-
-            System.out.println(subfile.getName() + " : " + length);
+            System.out.println(subfile.getAbsoluteFile() + " " + subfile.getTotalSpace());
         }
     }
 }
