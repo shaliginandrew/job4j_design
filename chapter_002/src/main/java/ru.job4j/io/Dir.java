89effc3709f887940603ef8b135131059d347bb8
shaliginandrew <shaligin.andrew@gmail.com>
2020-08-23 17:44:26 +0300

    File [#294220]

--- chapter_002/src/main/java/ru.job4j/io/Dir.java
+++ chapter_002/src/main/java/ru.job4j/io/Dir.java
@@ -1,9 +1,12 @@
 package ru.job4j.io;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Dir {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File file = new File("c:\\projects");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
@@ -11,9 +14,18 @@ public class Dir {
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        long sizeParentFolder = Files.walk(file.toPath())
+                .filter(p -> p.toFile().isFile())
+                .mapToLong(p -> p.toFile().length())
+                .sum();
+        System.out.println(file + " " + sizeParentFolder);
         for (File subfile : file.listFiles()) {
-            System.out.println(subfile.getAbsoluteFile() + " " + subfile.getTotalSpace());
+            long sizeChildrenFolder = Files.walk(subfile.toPath())
+                    .filter(p -> p.toFile().isFile())
+                    .mapToLong(p -> p.toFile().length())
+                    .sum();
+
+            System.out.println(subfile + " " + sizeChildrenFolder);
         }
     }
 }
