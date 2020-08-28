22c83a5d9304b0fb46835e2060a90aa9ae19c266
shaliginandrew <shaligin.andrew@gmail.com>
2020-08-28 11:46:47 +0300

    Кодировка. [#294224]

--- chapter_002/src/main/java/ru.job4j/io/Chat.java
+++ chapter_002/src/main/java/ru.job4j/io/Chat.java
@@ -2,52 +2,56 @@ package ru.job4j.io;
 
 
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Chat {
-    public static void main(String[] args) throws IOException {
-        String path = "./chapter_002/data/words.txt";
-        String str;
+    private String str;
+    private boolean flag = true;
+
+    public void dialog() {
+
         List<String> wordsList = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+        try (BufferedReader in = new BufferedReader(new FileReader("./chapter_002/data/words.txt"))) {
             wordsList = in.lines().collect(Collectors.toList());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        boolean flag = true;
-
-
-
-         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-             System.out.println("Введите слово: \n 'стоп' - программа замолкает, при этом можно продолжать отправлять сообщения в чат, \n 'продолжить' - программа снова начинает отвечать, \n 'закончить' - программа завершает работу ");
-             do {
-                 int index = (int) (Math.random() * (wordsList.size() - 1));
-                 str = br.readLine();
-                 if (!str.equals("стоп") && flag) {
-                     System.out.println(wordsList.get(index));
-
-                 }
-                 if (str.equals("стоп")) {
-                     flag = false;
-                 }
-
-                 if (str.equals("продолжить")) {
-                     flag = true;
-                     System.out.println(wordsList.get(index));
-                 }
-
-             } while (!str.equals("закончить"));
-
-         }  catch (Exception e) {
-        e.printStackTrace();
+        Scanner input = new Scanner(System.in);
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream("./chapter_002/data/log.txt")))) {
+            String text = "Введите слово: \n 'стоп' - программа замолкает, при этом можно продолжать отправлять сообщения в чат, \n 'продолжить' - программа снова начинает отвечать, \n 'закончить' - программа завершает работу ";
+            System.out.println(text);
+            out.println(text);
+            do {
+                int index = (int) (Math.random() * (wordsList.size()));
+                str = input.nextLine();
+                out.println(str);
+                if (!str.equals("стоп") && flag && !str.equals("закончить")) {
+                    System.out.println(wordsList.get(index));
+                    out.println(wordsList.get(index));
+                }
+                if (str.equals("стоп")) {
+                    flag = false;
+                }
+                if (str.equals("продолжить")) {
+                    flag = true;
+                    System.out.println(wordsList.get(index));
+                    out.println(wordsList.get(index));
+                }
+            } while (!str.equals("закончить"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-         }
 
+    public static void main(String[] args) throws IOException {
+     Chat chat = new Chat();
+     chat.dialog();
+    }
 }
+
+
 
