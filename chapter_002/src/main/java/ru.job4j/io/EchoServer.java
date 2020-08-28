9e97e2fb97ee7bbe1a8d5f12c1e534d926440b58
shaliginandrew <shaligin.andrew@gmail.com>
2020-08-28 22:12:34 +0300

    Бот [#294232]

--- chapter_002/src/main/java/ru.job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru.job4j/io/EchoServer.java
@@ -8,6 +8,12 @@ public class EchoServer {
 
     public static void main(String[] args) throws IOException {
         boolean startServer = true;
+        String result = "";
+        String result2 = "";
+        String[] resultAny = null;
+        boolean flagHello = false;
+        boolean flagAny = false;
+     //   boolean flag = true;
         try (ServerSocket server = new ServerSocket(9000)) {
             while (startServer) {
                 Socket socket = server.accept();
@@ -17,18 +23,36 @@ public class EchoServer {
                     String str;
 
                     while (!(str = in.readLine()).isEmpty())  {
-                        if (str.contains("/?msg=Bye")) {
+                        if (str.contains("/?msg=Exit")) {
                             startServer = false;
                         }
-                        System.out.println(str);
+                       // if (str.contains("/?msg=Hello")) {
+                         //   result = "Hello";
+                        //    flagHello = true;
+                       // }
+                        if (!str.contains("/?msg=Hello") && !flagAny) {
+                          result2 = str.split("=")[1].split(" ")[0];
+                          flagAny = true;
+                                                 }
+
+
+                      //  System.out.println(str);
 
                     }
-                    if (!str.contains("/?msg=Bye") && startServer) {
-                        out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                       // if (flagHello) {
+                           // out.write(result.getBytes());
+                           // flagHello = false;
+                      //  }
+                        if (flagAny) {
+                            out.write(result2.getBytes());
+                            flagAny = false;
+                        }
                     }
+
+
 
                 }
             }
         }
     }
-}
